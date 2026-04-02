e84abd18f1453c36d0f87d51dde6d672684c5d9a
Your Name <dhyun1543@gmail.com>
2026-04-01 22:31:04 -0500

    finished main backtester logic + visualizer

--- backtesting_engine/backtester.py
+++ backtesting_engine/backtester.py
@@ -6,8 +6,9 @@ from data_parser import csv_to_trading_states, csv_to_trades, get_all_symbols
 from trader import Trader
 from dotenv import load_dotenv
 from position_limits import position_limits_dict
-
+from visualizer import BacktestingResult
 load_dotenv()
+
 
 class Backtester:
     def __init__(self, order_book_csv_name: str, trades_csv_name: str):
@@ -17,14 +18,23 @@ class Backtester:
         self.trades.sort(key=lambda t: t.timestamp)
     def include_observations(self, observation_csv_name:str):
         pass
-    def run_trader(self, trader: Trader):
+    def run_trader(self, trader: Trader) -> BacktestingResult:
         own_trades: Dict[Symbol, List[Trade]] = {}
         market_trades: Dict[Symbol, List[Trade]] = {}
         position: Dict[Symbol, Position] = {}
         traderData: str = ""
-        trade_ptr = 0
-        pnl_history = []
+        trade_ptr: int = 0
+
+        # BACKTESTER METRICS 
+        pnl_realized: List[Dict[str: float]] = []
+        pnl_unrealized: List[Dict[str: float]] = []
+        position_history: List[Dict[str, int]] = []
+        fill_rate_history: List[Dict[str, float]] = []
+        drawdown_history: List[float] = []
         cash = {symbol: 0 for symbol in self.symbols}
+
+        # BACKTESTER LOGIC
+        peak_pnl: float = 0
         for symbol in self.symbols:
             own_trades[symbol] = []
             market_trades[symbol] = []
@@ -37,6 +47,7 @@ class Backtester:
             state.own_trades={k: v[:] for k, v in own_trades.items()}
             state.market_trades={k: v[:] for k, v in market_trades.items()}
             result, conversion, next_trader_data = trader.run(state)
+            fill_ratios:Dict[str, float] = {}
             # Error catch result (make sure its in the proper return format, as listed in the IMC Prosperity docs)
             for symbol in self.symbols:
                 try:
@@ -67,8 +78,10 @@ class Backtester:
             # Update positions & fill orders
             for symbol in self.symbols:
                 # check position limits first
-                long_sum=0
-                short_sum=0
+                fill_ratios[symbol]=0.0
+                ratio_denominator: float = 0.0
+                long_sum: float=0
+                short_sum:float=0
                 for order in result[symbol]:
                     if order.quantity>0:
                        long_sum+=order.quantity
@@ -87,12 +100,14 @@ class Backtester:
                 asks_sorted.sort()
                 bids_sorted.sort(reverse=True)
                 old_quantity = position[symbol]
+                
                 for order in result[symbol]:
                     if order.quantity < 0: #short
                         #check limit enforcement
                         if old_quantity + short_sum > position_limits_dict[symbol]:
                             continue
                         total_request = abs(order.quantity)
+                        ratio_denominator+=total_request
                         while total_request > 0 and len(bids_sorted) != 0 and bids_sorted[0][0] >= order.price:
                              delta = min(bids_sorted[0][1], total_request)
                              total_request-=delta
@@ -101,11 +116,13 @@ class Backtester:
                              own_trades[symbol].append(Trade(symbol, bids_sorted[0][0], -delta, "", "SUBMISSION", state.timestamp))
                              if bids_sorted[0][1] == 0:
                                 del bids_sorted[0]
+                        fill_ratios[symbol]+=abs(order.quantity)-total_request
                     else: #long
                         #check limit enforcement
                         if old_quantity + long_sum > position_limits_dict[symbol]:
                             continue
                         total_request = order.quantity
+                        ratio_denominator+=total_request
                         while total_request > 0 and len(asks_sorted) != 0 and asks_sorted[0][0] <= order.price:
                              delta = min(abs(asks_sorted[0][1]), total_request)
                              total_request-=delta
@@ -114,17 +131,21 @@ class Backtester:
                              own_trades[symbol].append(Trade(symbol, asks_sorted[0][0], delta, "SUBMISSION", "", state.timestamp))
                              if asks_sorted[0][1] == 0:
                                 del asks_sorted[0]
-
-
+                        fill_ratios[symbol]+=order.quantity-total_request
+                fill_ratios[symbol]/=max(1,ratio_denominator) # div by zero err
+            t_pnl:float = 0
             for symbol in self.symbols:
                 for trade in own_trades[symbol]:
                     if trade.buyer == "SUBMISSION":
                         cash[symbol] -= trade.quantity * trade.price
                     else:
                         cash[symbol] += abs(trade.quantity) * trade.price
-
+            
+            for symbol in self.symbols:
+                t_pnl+=cash[symbol]
+            
                 
-                # Computing PNL (Going to change in the future)
+            # Computing PNL (Going to change in the future)
             current_pnl = {}
             for symbol in self.symbols:
                 mid_price = None
@@ -137,12 +158,19 @@ class Backtester:
                 elif asks:
                     mid_price = min(asks)
                 else:
-                    mid_price = 0  # fallback
+                    mid_price = 0
 
-                current_pnl[symbol] = cash[symbol] + position[symbol] * mid_price
-    
-            pnl_history.append(current_pnl)
-        return position, pnl_history
+                current_pnl[symbol] = position[symbol] * mid_price
+                t_pnl+=current_pnl[symbol]
+            # Push metrics
+            pnl_unrealized.append(current_pnl)
+            pnl_realized.append(cash.copy())
+            position_history.append(position.copy())
+            fill_rate_history.append(fill_ratios)
+            peak_pnl = max(t_pnl, peak_pnl)
+            drawdown_history.append(peak_pnl-t_pnl)
+            
+        return BacktestingResult(position_history, pnl_unrealized, pnl_realized, fill_rate_history, drawdown_history)
             
 
             
---
+++ backtesting_engine/visualizer.py
@@ -0,0 +1,90 @@
+from datamodel import TradingState, Listing, OrderDepth, Observation, Symbol, Trade, Position
+import pandas as pd
+import os
+from typing import Dict, List
+from data_parser import csv_to_trading_states, csv_to_trades, get_all_symbols
+from trader import Trader
+from dotenv import load_dotenv
+from position_limits import position_limits_dict
+import matplotlib.pyplot as plt
+
+load_dotenv()
+
+
+class BacktestingResult:
+    def __init__(self, position_history: List[Dict[str, int]], pnl_realized: List[Dict[str, float]], pnl_unrealized: List[Dict[str, float]], fill_rate_history: List[Dict[str, float]], drawdown_history: List[float]):
+        self.position_history: List[Dict[str, int]]=position_history
+        self.pnl_realized: List[Dict[str, float]]=pnl_realized
+        self.pnl_unrealized: List[Dict[str, float]]=pnl_unrealized
+        self.currency_medium: str = os.getenv("DENOMINATION", "XIRECS")
+        self.fill_ratios = fill_rate_history
+        self.drawdown_history=drawdown_history
+        self.timestamps: List[int] = [i for i in range(len(self.position_history))]
+        # graphing dictionary
+        self.graphing_functions = {
+            "inventory": self.graph_inventory
+        }
+        self.symbols: List[str] = []
+        for symbol in position_history[0]:
+            self.symbols.append(symbol)
+        
+    def set_symbol_colors(self, colors: Dict[str, str]) -> None:
+        self.colors=colors
+    def graph_inventory(self, graph_alone_flag: bool = True, units: tuple[str] = ("TIMESTAMP", "POSITION AMOUNT"))->None:
+        for symbol in self.symbols:
+            arr: List[float] = []
+            for t in range(len(self.position_history)):
+                arr.append(self.position_history[t][symbol])
+            plt.plot(self.timestamps, arr, color=self.colors[symbol], label=symbol)
+        if graph_alone_flag == True:
+            plt.legend()
+            plt.xlabel(units[0])
+            plt.ylabel(units[1])
+            plt.show()
+    def graph_fill_ratios(self, graph_alone_flag: bool = True, units: tuple[str] = ("TIMESTAMP", "RATIO"))->None:
+        for symbol in self.symbols:
+            arr: List[float] = []
+            for t in range(len(self.fill_ratios)):
+                arr.append(self.fill_ratios[t][symbol])
+            plt.plot(self.timestamps, arr, color=self.colors[symbol], label=symbol)
+        if graph_alone_flag == True:
+            plt.legend()
+            plt.xlabel(units[0])
+            plt.ylabel(units[1])
+            plt.show()
+    def graph_drawdown(self, graph_alone_flag: bool = True) -> None:
+        plt.plot(self.timestamps, self.drawdown_history, color="red", label="Drawdown in " + self.currency_medium + "")
+        if graph_alone_flag == True:
+            plt.legend()
+            plt.xlabel("TIMESTAMP")
+            plt.ylabel(self.currency_medium)
+            plt.show()
+    def graph_pnl(self, exclude_other_pnl: bool = True, colors: tuple[str] = ("green", "purple", "blue"), graph_alone_flag: bool = True)->None:
+        r_pnl: List[float] = []
+        t_pnl: List[float] = []
+        u_pnl: List[float] = []
+        for t in range(len(self.position_history)):
+            r_pnl_sum: float = 0
+            u_pnl_sum: float = 0
+            for symbol in self.symbols:
+               r_pnl_sum+=self.pnl_realized[t][symbol]
+               u_pnl_sum+=self.pnl_unrealized[t][symbol]
+            r_pnl.append(r_pnl_sum)
+            u_pnl.append(u_pnl_sum)
+            t_pnl.append(r_pnl_sum+u_pnl_sum)
+        
+        
+        plt.plot(self.timestamps, t_pnl, color=colors[1], label="Total PNL")
+        if exclude_other_pnl == False:
+            plt.plot(self.timestamps, r_pnl, color=colors[0], label="Total Realized PNL")
+            plt.plot(self.timestamps, u_pnl, color=colors[2], label="Total Unrealized PNL")
+        if graph_alone_flag == True:
+            plt.legend()
+            plt.xlabel("TIMESTAMP")
+            plt.ylabel(self.currency_medium)
+            plt.show()
+    
+    def output_graphs(self, func_list: List[str]) -> None:
+        pass
+    def __str__(self) -> str:
+        return "BACKTESTING RESULT OF " + str(len(self.position_history)) + " UNIQUE TIMESTAMPS."
